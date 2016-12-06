41340b1e18dfa004a2c4acca901339f442b4f032
kid-sugoi <hethtran@ucsc.edu>
2016-12-05 17:01:25 -0800

    Add files via upload
    needs adjustments around heuristics. still throws a weird error sometimes

--- campaign_crafter.py
+++ campaign_crafter.py
@@ -1,14 +1,14 @@
 import yaml
 from collections import namedtuple, defaultdict, OrderedDict
-from heapq import heappop, heappush
+from heapq import heappop, heappush, heapify
 from math import inf
 import copy
 
 event_Cap = 5
-Event = namedtuple('Event', ['name', 'check', 'effect'])
+Event = namedtuple('Event', ['name', 'check', 'effect', 'value'])
 
 
-class Party_State(dict):
+class Party_State(OrderedDict):
 
     def __key(self):
         return tuple(list(self.items()))
@@ -17,9 +17,9 @@ class Party_State(dict):
         return hash(self.__key())
 
     def __lt__(self, other):
-        return self.__key() < other.__key()
+        return self.__key() > other.__key()
 
-
+#takes all the components in party state and consolidates them into a single list for comparison
 def mega_list(Party_State):
     s_m_l = []
     for group in Party_State:
@@ -62,7 +62,8 @@ def make_effector(rule):
             if r == 'results':
                 for result in rule[r]:
                     print(result)
-                    next_state['catalog'].append(result)
+                    if result not in next_state['catalog']:
+                        next_state['catalog'].append(result)
 
         print("2.a:", state)
         print("2.b:", next_state)
@@ -78,7 +79,6 @@ def make_goal_checker(goal):
 
     def is_goal(state):
         # This code is used in the search process and may be called millions of times.
-        print("inside is goal", state)
         condition = True
         state_mega_list = mega_list(state)
 
@@ -91,13 +91,29 @@ def make_goal_checker(goal):
     return is_goal
 
 
+def make_heuristic_calculator(rule, goal):
+
+    def auto_heuristic(state):
+        heuristic_value = 0
+        state_mega_list = mega_list(state)
+        for r in rule:
+            if r == 'reqs':
+                for component in rule[r]:
+                    if component in goal:
+                        heuristic_value += 1
+
+
+        return heuristic_value
+
+    return auto_heuristic
+
 def graph(state):
     # Iterates through all recipes/rules, checking which are valid in the given state.
     # If a rule is valid, it returns the rule's name, the resulting state after application
     # to the given state, and the cost for the rule.
     for r in all_events:
         if r.check(state):
-            yield (r.name, r.effect(state))
+            yield (r.name, r.effect(state), r.value(state))
 
 
 def heuristic(previous_state, action_taken, goals):
@@ -115,7 +131,8 @@ def search(graph, state, is_goal, limit, heuristic):
     number_of_state_visits = 0
 
     # The priority queue
-    queue = [state]
+    queue = [(0,state)]
+    heapify(queue)
     goals = event_Crafting["Goals"]
 
     # The dictionary that will be returned with the event lengths
@@ -135,11 +152,14 @@ def search(graph, state, is_goal, limit, heuristic):
     # in the path and the action that took you to this state
     while queue:
         print("queue at start of loop:", queue)
-        current_state = heappop(queue)
+        current_heuristic_cost, current_state = heappop(queue)
         if is_goal(current_state):
             print("Solution found")
             if best_result_so_far != None:
-                if length_costs[best_result_so_far] < length_costs[current_state] < event_Cap:
+                print(tuple(mega_list(best_result_so_far)))
+                print(tuple(mega_list(current_state)))
+                if length_costs[tuple(mega_list(best_result_so_far))] < length_costs[tuple(mega_list(current_state))] \
+                        < event_Cap:
                     best_result_so_far = current_state
                     iterations_at_best_result = number_of_state_visits
             else:
@@ -150,36 +170,36 @@ def search(graph, state, is_goal, limit, heuristic):
 
         for gva in graph_valid_actions:
             number_of_state_visits += 1
-            print("gva start", number_of_state_visits)
+            print("GVA START", number_of_state_visits)
+            print(gva)
             heuristic_value = heuristic(current_state, gva, goals)
-            print(heuristic_value)
-            #new_heuristic_cost = current_heuristic_cost + gva[2] + heuristic_value
+            new_heuristic_cost = current_heuristic_cost + gva[2] + heuristic_value
 
-
+            print("length", length_costs)
             if tuple(mega_list(gva[1])) not in length_costs:
+                length_costs[tuple(mega_list(gva[1]))] = length_costs[tuple(mega_list(current_state))] + 1
                 backpointers[tuple(mega_list(gva[1]))] = current_state
-                #heappush(queue, gva[1])
-                queue.append(gva[1])
-                print(queue)
-                print(gva[1], "mandate")
-            print("gva end", number_of_state_visits)
+                heapify(queue)
+                heappush(queue,(new_heuristic_cost , gva[1]))
+                print("QUEUE:", queue)
+            print("GVA END", number_of_state_visits)
 
     if best_result_so_far and is_goal(best_result_so_far):
         print("Goal reached")
         print("Final state: ", best_result_so_far)
-        print("Final Length: ", length_costs[tuple(best_result_so_far)])
+        print("Final Length: ", length_costs[tuple(mega_list(best_result_so_far))])
         print("Number of State Visits at best solution found: ", iterations_at_best_result)
 
-        while backpointers[best_result_so_far] != None:
-            print("Path: ", backpointers[best_result_so_far])
-            best_result_so_far = backpointers[best_result_so_far]
+        while backpointers[tuple(mega_list(best_result_so_far))] != None:
+            print("Path: ", tuple(mega_list(best_result_so_far)))
+            best_result_so_far = backpointers[tuple(mega_list(best_result_so_far))]
                 # print("Iterations: ", iterations)
         return None
-    else:
-            # Failed to find a path
-        print("Number of states searched ", number_of_state_visits)
-        print("Failed to find a path from", state, 'within time limit.')
-        return None
+
+    # Failed to find a path
+    print("Number of states searched ", number_of_state_visits)
+    print("Failed to find a path from", state, 'within time limit.')
+    return None
 
 
 if __name__ == '__main__':
@@ -190,30 +210,32 @@ if __name__ == '__main__':
     #print('All Locations:', event_Crafting['Location'])
 
     # List of items in your initial inventory with amounts:
-    print('Initial Party State:', event_Crafting['Party_State'])
+    #print('Initial Party State:', event_Crafting['Party_State'])
 
     # Dict of crafting recipes (each is a dict):
     ## print('Example event:','Gossip at the bar for rumors ->', event_Crafting['Event']['Gossip at the bar for rumors'])
     party_Status = Party_State()
     party_Status = event_Crafting['Party_State']
 
+    # Create a function which checks for the goal
+    goals = event_Crafting['Goals']
+    is_goal = make_goal_checker(goals)
+    print("goals", goals)
+
     # Build rules
     all_events = []
     for name, rule in event_Crafting['Event'].items():
         checker = make_checker(rule)
         effector = make_effector(rule)
-        event = Event(name, checker, effector)
+        heuristic_calc = make_heuristic_calculator(rule, goals)
+        event = Event(name, checker, effector, heuristic_calc)
         all_events.append(event)
 
     #sets party_Status to the dictionary of lists defined in yaml
-
     print("party", party_Status['party'])
     print("location", party_Status['location'])
     print("catalog", party_Status['catalog'])
 
-    # Create a function which checks for the goal
-    is_goal = make_goal_checker(event_Crafting['Goals'])
-    print("goals", event_Crafting['Goals'])
 
     # Search for a solution
     resulting_plan = search(graph, party_Status, is_goal, 600, heuristic)
